e70d7e831bb7d0b6baab06344462e8ad7439f071
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-05 23:47:44 +0600

    first commit

---
+++ task1.js
@@ -0,0 +1,8 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(a,b,c,d){
+    const result = a*b*c*d;
+    return result;
+}
+const multiplyResult=multiply(1,2,3,4);
+console.log(multiplyResult);
